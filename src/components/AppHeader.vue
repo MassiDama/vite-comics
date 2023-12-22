fc2e097af4e861717ecd8ca58dcefc80c5a561d1
Massidama <massidama@outlook.it>
2023-12-22 16:04:30 +0100

    feat: struttura componenti

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          current: true
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "FANS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          current: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+    <header>
+      <div class="container">
+        <div class="logo">
+        
+        </div>
+      </div>
+      
+    </header>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+@use '../styles/general.scss' as *;
+
+</style>
