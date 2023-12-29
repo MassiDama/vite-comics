0b393d927289e0ceeb466b06506f8a01d6b9984b
Massidama <massidama@outlook.it>
2023-12-29 13:22:13 +0100

    feat: struttura contatti social, logo-footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -39,56 +39,83 @@ export default {
 
        <div class="container">
               <!-- parte info section -->
-        <div class="info-section">
-            <h4> DC COMICS </h4>
-            <ul>
-                <li><a href="#"> Characters </a></li>
-                <li><a href="#"> Comics </a></li>
-                <li><a href="#"> Movies </a></li>
-                <li><a href="#"> TV </a></li>
-                <li><a href="#"> Games </a></li>
-                <li><a href="#"> Videos </a></li>
-                <li><a href="#"> News </a></li>
-            </ul>
+            <div class="info-content">
+                <div class="info-section">
+                    <h4> DC COMICS </h4>
+                    <ul>
+                        <li><a href="#"> Characters </a></li>
+                        <li><a href="#"> Comics </a></li>
+                        <li><a href="#"> Movies </a></li>
+                        <li><a href="#"> TV </a></li>
+                        <li><a href="#"> Games </a></li>
+                        <li><a href="#"> Videos </a></li>
+                        <li><a href="#"> News </a></li>
+                    </ul>
 
-            <h4> SHOP </h4>
-            <ul>
-                <li><a href="#"> Shop DC </a></li>
-                <li><a href="#"> Shop DC Collectibles </a></li>
-            </ul>
-        </div>
+                    <h4> SHOP </h4>
+                    <ul>
+                        <li><a href="#"> Shop DC </a></li>
+                        <li><a href="#"> Shop DC Collectibles </a></li>
+                    </ul>
+                </div>
 
-        <!-- seconda colonna info section -->
-        <div class="info-section">
-            <h4> DC COMICS </h4>
-            <ul>
-                <li><a href="#"> Terms Of Use </a></li>
-                <li><a href="#"> Privacy policy (New) </a></li>
-                <li><a href="#"> Ad Choices </a></li>
-                <li><a href="#"> Advertising </a></li>
-                <li><a href="#"> Jobs </a></li>
-                <li><a href="#"> Subscriptions </a></li>
-                <li><a href="#"> Talent Workshops </a></li>
-                <li><a href="#"> CPSC Certificates </a></li>
-                <li><a href="#"> Ratings </a></li>
-                <li><a href="#"> Shop Help </a></li>
-                <li><a href="#"> Contact Us </a></li>
-            </ul>
-        </div>
+                <!-- seconda colonna info section -->
+                <div class="info-section">
+                    <h4> DC </h4>
+                    <ul>
+                        <li><a href="#"> Terms Of Use </a></li>
+                        <li><a href="#"> Privacy policy (New) </a></li>
+                        <li><a href="#"> Ad Choices </a></li>
+                        <li><a href="#"> Advertising </a></li>
+                        <li><a href="#"> Jobs </a></li>
+                        <li><a href="#"> Subscriptions </a></li>
+                        <li><a href="#"> Talent Workshops </a></li>
+                        <li><a href="#"> CPSC Certificates </a></li>
+                        <li><a href="#"> Ratings </a></li>
+                        <li><a href="#"> Shop Help </a></li>
+                        <li><a href="#"> Contact Us </a></li>
+                    </ul>
+                </div>
 
-        <!-- terza colonna info section -->
-        <div class="info-section">
-            <h4> DC COMICS </h4>
-            <ul>
-                <li><a href="#"> DC </a></li>
-                <li><a href="#"> MAD Magazine </a></li>
-                <li><a href="#"> DC Kids </a></li>
-                <li><a href="#"> DC Universe </a></li>
-                <li><a href="#"> DC Power Visa </a></li>
-            </ul>
-        </div>
+                <!-- terza colonna info section -->
+                <div class="info-section">
+                    <h4> SITES </h4>
+                    <ul>
+                        <li><a href="#"> DC </a></li>
+                        <li><a href="#"> MAD Magazine </a></li>
+                        <li><a href="#"> DC Kids </a></li>
+                        <li><a href="#"> DC Universe </a></li>
+                        <li><a href="#"> DC Power Visa </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="logo-footer">
+                
+            </div>
        </div>
 
+      
+
+    </section>
+
+    <!-- barra finale contatti social -->
+    <section class="gray-bar">
+        <div class="container">
+
+            <div class="sign-up">
+                SIGN-UP NOW!
+            </div>
+
+            <div class="social-contact">
+                <span> FOLLOW US</span>
+                <a href="#"><img src="/img/footer-facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="/img/footer-twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="/img/footer-youtube.png" alt="YouTube"></a>
+                <a href="#"><img src="/img/footer-pinterest.png" alt="Pinterest"></a>
+                <a href="#"><img src="/img/footer-periscope.png" alt="Periscope"></a>
+            </div>
+        </div>
     </section>
 
    
@@ -100,6 +127,7 @@ export default {
 @use '../styles/partials/variables' as *;
 @use '../styles/general.scss' as *;
 
+     // stili barra blu, prima parte footer (info)
     .blue-bar {
         width: 100%;
         background-color: $primary;
@@ -129,15 +157,22 @@ export default {
         }
     }
 
+
+    // stili parte info-section
     .footer-info {
         background-image: url(../../public/img/footer-bg.jpg);
         width: 100%;
-        height: 500px;
+        height: 350px;
         
         .container {
             display: flex;
+            justify-content: space-between;
+            height: 100%;
 
-            .info-section {
+            .info-content {
+                display: flex;
+
+                .info-section {
                 padding: 30px 10px 0px 0;
 
                 h4 {
@@ -150,8 +185,50 @@ export default {
                     font-size: 12px;
                 }
             }
+            }
+
+            .logo-footer {
+                width: 450px;
+                height: 100%;
+                background-image: url('../../public/img/dc-logo-bg.png');
+                background-size: cover;
+                background-position-y: 40%;
+            } 
 
         }
+    }
+
+    // stili barra finle con contatti social e sign-up
+    .gray-bar {
+        width: 100%;
+        height: 80px;
+        background-color: #303030;
+
+       .container {
+        @include flex-between();
+        height: 100%;
+
+        .sign-up {
+            border: 2px solid $primary;
+            padding: 10px 20px;
+            color: white;
+        }
+
+        .social-contact {
+            @include flex-between();
+
+            span {
+                color: $primary;
+
+            }
+
+            a {
+                padding-left: 10px;
+            }
+        }
+
+       }
+
     }
     
 
